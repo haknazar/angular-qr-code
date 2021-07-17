78acc436d8406e1363bd0568e04f51169eb93280
Haknazar Amangeldiyev <a.haknazar95@gmail.com>
2021-07-17 21:28:09 +0800

    ui changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgxQRCodeModule } from '@techiediaries/ngx-qrcode';
 import { FormsModule } from '@angular/forms';
-
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, NgxQRCodeModule, FormsModule
+    AppRoutingModule, NgxQRCodeModule, FormsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
